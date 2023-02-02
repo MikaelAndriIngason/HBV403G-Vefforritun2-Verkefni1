9344964c6586cbfda6ff5bc03bdad1a3c94a5479
MikaelAndriIngason <mikaelin258@nemi.tskoli.is>
2023-02-02 22:38:17 +0000

    test 4

--- src/test/fileTypes.test.js
+++ src/test/fileTypes.test.js
@@ -1,4 +1,17 @@
-/*import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import { readCSV, readJson, checkURL } from '../lib/fileTypes.js'
 
-describe('')*/
+describe('checkURL', () => {
+    it('Returns true if the link is valid', () => {
+        expect(checkURL('https://www.google.com/')).toBe(true)
+    })
+    it('Returns false if the lin is not valid', () => {
+        expect(checkURL('google.com')).toBe(false)
+    })
+})
+
+/*describe('readJson', () => {
+    it('Reads a json file and returns a json object, if there is a file', () => {
+        expect(readJson('./src/test/testfiles/readjsontest.json')).resolves.toBe()
+    })
+})*/
